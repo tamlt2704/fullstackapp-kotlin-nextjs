342691cadfa6ed812181274535e22cad49a19181
La Thanh Tam <tamlt2704@gmail.com>
2025-11-30 15:44:45 +0000

    hello controller

---
+++ src/main/kotlin/HelloController.kt
@@ -0,0 +1,13 @@
+package org.example
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello World!"
+    }
+}
