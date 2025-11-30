99d22353195639ebb61609d9bd9e6210afc50dd0
La Thanh Tam <tamlt2704@gmail.com>
2025-11-30 16:13:16 +0000

    fix entity error

--- src/main/kotlin/org/example/entity/User.kt
+++ src/main/kotlin/org/example/entity/User.kt
@@ -4,18 +4,18 @@ import jakarta.persistence.*
 
 @Entity
 @Table(name = "users")
-data class User(
+data class User @JvmOverloads constructor(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
     
     @Column(unique = true)
-    val username: String,
+    val username: String = "",
     
-    val password: String,
+    val password: String = "",
     
     @Enumerated(EnumType.STRING)
-    val role: Role
+    val role: Role = Role.USER
 )
 
 enum class Role {
